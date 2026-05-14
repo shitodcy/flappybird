42c7923959f859d7a97dc0c2ef281530499defdb
azunya <tn.putra404.com>
2026-05-14 08:51:43 +0700

    mengubah control bird

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-const GRAVITY : int = 1000
-const MAX_VEL : int = 600
-const FLAP_SPEED : int = -400
+const GRAVITY : int = 800
+const MAX_VEL : int = 600      
+const FLAP_SPEED : int = -260
 var flying : bool = false
 var falling : bool = false
 const START_POS = Vector2(100, 400)
@@ -23,12 +23,8 @@ func _physics_process(delta):
 		if velocity.y > MAX_VEL:
 			velocity.y = MAX_VEL
 		if flying:
-			# Menghitung target rotasi berdasarkan kecepatan jatuh
 			var target_rotation = deg_to_rad(velocity.y * 0.05)
-			
-			# Mengubah rotasi perlahan (0.1 adalah beban beratnya, makin kecil makin lambat)
-			rotation = lerp_angle(rotation, target_rotation, 0.1)
-			
+			set_rotation(deg_to_rad(velocity.y * 0.02))
 			$AnimatedSprite2D.play()
 		elif falling:
 			set_rotation(PI/2)
@@ -38,9 +34,7 @@ func _physics_process(delta):
 		$AnimatedSprite2D.stop()
 			
 func flap():
-	# Menambah kecepatan ke atas
-	velocity.y += FLAP_SPEED 
+	velocity.y = FLAP_SPEED
 	
-	# Batas agar tidak melesat terlalu cepat ke atas
 	if velocity.y < FLAP_SPEED:
 		velocity.y = FLAP_SPEED
