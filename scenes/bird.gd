8347c6e635da79ca9db85995d49824dead909df0
EgidiusDicky <dickynarendra8@gmail.com>
2026-05-16 16:24:33 +0700

    feat(bird): add rapid alpha flashing effect during immunity

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -7,6 +7,9 @@ var flying : bool = false
 var falling : bool = false
 const START_POS = Vector2(100, 400)
 
+var immune_blink_timer : float = 0.0
+var is_immune_visual : bool = false
+
 func _ready():
 	reset()
 	
@@ -32,9 +35,23 @@ func _physics_process(delta):
 		move_and_collide(velocity * delta)
 	else:
 		$AnimatedSprite2D.stop()
+		
+	# Blink effect selama immune
+	if is_immune_visual:
+		immune_blink_timer += delta
+		if int(immune_blink_timer * 10) % 2 == 0:
+			modulate.a = 0.3
+		else:
+			modulate.a = 1.0
 			
 func flap():
 	velocity.y = FLAP_SPEED
 	
 	if velocity.y < FLAP_SPEED:
 		velocity.y = FLAP_SPEED
+
+func set_immune_visual(immune: bool):
+	is_immune_visual = immune
+	immune_blink_timer = 0.0
+	if not immune:
+		modulate.a = 1.0
